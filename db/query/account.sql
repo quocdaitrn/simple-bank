8b9eb7db00b6b442878ceeb6ff81a47c04b236ca
DaiTran <daitq.cntt@gmail.com>
2023-02-12 22:17:39 +0700

    Techschool Lecture 7->8

--- db/query/account.sql
+++ db/query/account.sql
@@ -9,6 +9,10 @@ INSERT INTO accounts (
 SELECT * FROM accounts
 WHERE id = $1 LIMIT 1;
 
+-- name: GetAccountForUpdate :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1 FOR NO KEY UPDATE;
+
 -- name: ListAccounts :many
 SELECT * FROM accounts
 WHERE owner = $1
@@ -20,6 +24,12 @@ LIMIT $3;
 UPDATE accounts
 SET balance = $2
 WHERE id = $1
+RETURNING *;
+
+-- name: AddAccountBalance :one
+UPDATE accounts
+SET balance = balance + sqlc.arg(amount)
+WHERE id = sqlc.arg(id)
 RETURNING *;
 
 -- name: DeleteAccount :exec
