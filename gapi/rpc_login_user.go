a095e0d3ef266f75247c91d928ac03acd167ea2b
DaiTran <daitq.cntt@gmail.com>
2023-02-24 15:50:22 +0700

    add validation

--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
@@ -11,9 +12,15 @@ import (
 	db "github.com/quocdaitrn/simple-bank/db/sqlc"
 	"github.com/quocdaitrn/simple-bank/pb"
 	"github.com/quocdaitrn/simple-bank/util"
+	"github.com/quocdaitrn/simple-bank/val"
 )
 
 func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (*pb.LoginUserResponse, error) {
+	violations := validateLoginUserRequest(req)
+	if violations != nil {
+		return nil, invalidArgumentError(violations)
+	}
+
 	user, err := server.store.GetUser(ctx, req.GetUsername())
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -66,4 +73,16 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 		RefreshTokenExpiresAt: timestamppb.New(refreshPayload.ExpiredAt),
 	}
 	return rsp, nil
+}
+
+func validateLoginUserRequest(req *pb.LoginUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := val.ValidateUsername(req.GetUsername()); err != nil {
+		violations = append(violations, fieldViolation("username", err))
+	}
+
+	if err := val.ValidatePassword(req.GetPassword()); err != nil {
+		violations = append(violations, fieldViolation("password", err))
+	}
+
+	return violations
 }
