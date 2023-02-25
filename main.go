75461066b4e1cbb780212c1762e7d84e6d995c7c
DaiTran <daitq.cntt@gmail.com>
2023-02-25 09:52:03 +0700

    lecture 52: grpc interceptor

--- main.go
+++ main.go
@@ -126,7 +126,7 @@ func runGatewayServer(config util.Config, store db.Store) {
 		log.Fatal().Err(err).Msg("cannot create listener")
 	}
 
-	log.Printf("start HTTP gateway server at %s", listener.Addr().String())
+	log.Info().Msgf("start HTTP gateway server at %s", listener.Addr().String())
 	err = http.Serve(listener, mux)
 	if err != nil {
 		log.Fatal().Err(err).Msg("cannot start HTTP gateway server")
